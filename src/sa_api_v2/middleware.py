ae74472ec6e84b975515a0749579057d683c9a1f
jalmogo <you@example.com>
2019-04-09 14:49:07 -0700

    chore(middleware): upgrade middleware

--- src/sa_api_v2/middleware.py
+++ src/sa_api_v2/middleware.py
@@ -6,74 +6,48 @@ import logging
 # Request logging examples:
 # https://github.com/Rhumbix/django-request-logging/blob/master/request_logging/middleware.py
 # https://gist.github.com/SehgalDivij/1ca5c647c710a2c3a0397bce5ec1c1b4
-class RequestBodyLogger (object):
+def RequestBodyLogger(get_response):
     logger = logging.getLogger('ms_api.request')
     request_body_methods = ['post', 'put', 'patch', 'delete']
 
-    def process_request(self, request):
+    def middleware(request):
         method = request.method.lower()
-        if method in self.request_body_methods:
+        if method in request_body_methods:
             body = ""
             if request.META.get('CONTENT_TYPE') == 'application/json' and\
                request.body != '' and request.body is not None:
                 # DELETE often has an empty string as body, so we've checked for that here.
                 body = json.dumps(json.loads(request.body.decode("utf-8")), indent=2)
-            self.logger.info('"{} {}" {}'.format(
+            logger.info('"{} {}" {}'.format(
                 request.method,
                 request.get_full_path(),
                 body
             ))
+        return get_response(request)
+    return middleware
 
 
-class RequestTimeLogger (object):
-    def process_request(self, request):
-        self.start_time = time.time()
+def RequestTimeLogger(get_response):
+    # One-time configuration and initialization.
+    logger = logging.getLogger('utils.request_timer')
 
-    def process_response(self, request, response):
-        # NOTE: If there was some exception, or some other reason that the
-        # process_request method was not called, we won't know the start time.
-        # Check that we know it first.
-        if hasattr(self, 'start_time'):
-            duration = time.time() - self.start_time
+    def middleware(request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        start_time = time.time()
 
-            # Log the time information
-            logger = logging.getLogger('utils.request_timer')
-            logger.debug('(%0.3f) "%s %s" %s' % (
-                duration,
-                request.method,
-                request.get_full_path(),
-                response.status_code
-            ))
+        response = get_response(request)
 
-        return response
+        duration = time.time() - start_time
 
-
-class CookiesLogger (object):
-    """
-    Logs in the request and response.
-    """
-    def process_response(self, request, response):
-        logger = logging.getLogger('utils.cookies_logger')
-        logger.debug(
-            '(%s)\n'
-            '\n'
-            'Request cookies: %s\n'
-            '\n'
-            'Response cookies: %s' % (
-                response.status_code, 
-                request.COOKIES, 
-                response.cookies or {}
+        # Log the time information
+        logger.debug('(%0.3f) "%s %s" %s' % (
+            duration,
+            request.method,
+            request.get_full_path(),
+            response.status_code
         ))
-        return response
-
-
-class JSEnableAllCookies (object):
-    """
-    Removes the httponly flag from all the cookies being set.
-    """
-    def process_response(self, request, response):
-        if response.cookies:
-            for morsel in response.cookies.values():
-                morsel['httponly'] = ''
 
         return response
+
+    return middleware
