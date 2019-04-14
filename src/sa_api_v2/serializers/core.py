2ba0f7a4cdb8350a23cb0948d98e86ccf06b8393
jalmogo <you@example.com>
2019-04-13 23:37:11 -0700

    refactor(serializers/fields): separate module

--- src/sa_api_v2/serializers/core.py
+++ src/sa_api_v2/serializers/core.py
@@ -2,21 +2,36 @@
 DjangoRestFramework resources for the Shareabouts REST API.
 """
 from django.utils import six
-import ujson as json
 import re
-from collections import defaultdict, OrderedDict
-from django.contrib.gis.geos import GEOSGeometry
+from collections import defaultdict
 from django.core.exceptions import ValidationError
-from django.utils.http import urlquote_plus
-from rest_framework import pagination, serializers, fields
+from rest_framework import pagination, serializers
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 from .mixins import (
     ActivityGenerator,
     EmptyModelSerializer,
     DataBlobProcessor,
     AttachmentSerializerMixin,
+)
+
+from .fields import (
+    GeometryField,
+    DataSetRelatedField,
+    UserRelatedField,
+    PlaceRelatedField,
+    SubmissionSetRelatedField,
+    TagRelatedField,
+    PlaceIdentityField,
+    AttachmentIdentityField,
+    SubmissionSetIdentityField,
+    DataSetPlaceSetIdentityField,
+    DataSetSubmissionSetIdentityField,
+    SubmissionIdentityField,
+    TagIdentityField,
+    PlaceTagListIdentityField,
+    PlaceTagIdentityField,
+    DataSetIdentityField,
 )
 from .. import apikey
 from .. import cors
@@ -33,339 +48,12 @@ logger = logging.getLogger(__name__)
 
 ###############################################################################
 #
-# Geo-related fields
-# ------------------
-#
-
-class GeometryField(serializers.Field):
-    def __init__(self, format='dict', *args, **kwargs):
-        self.format = format
-
-        if self.format not in ('json', 'wkt', 'dict'):
-            raise ValueError('Invalid format: %s' % self.format)
-
-        super(GeometryField, self).__init__(*args, **kwargs)
-
-    def to_representation(self, obj):
-        if self.format == 'json':
-            return obj.json
-        elif self.format == 'wkt':
-            return obj.wkt
-        elif self.format == 'dict':
-            return json.loads(obj.json)
-        else:
-            raise ValueError('Cannot output as %s' % self.format)
-
-    def to_internal_value(self, data):
-        if not isinstance(data, basestring):
-            data = json.dumps(data)
-
-        try:
-            return GEOSGeometry(data)
-        except Exception as exc:
-            raise ValidationError('Problem converting native data to Geometry: %s' % (exc,))
-
-###############################################################################
-#
-# Shareabouts-specific fields
-# ---------------------------
-#
-
-
-class ShareaboutsFieldMixin (object):
-
-    # These names should match the names of the cache parameters, and should be
-    # in the same order as the corresponding URL arguments.
-    url_arg_names = ()
-
-    def get_url_kwargs(self, obj):
-        """
-        Pull the appropriate arguments off of the cache to construct the URL.
-        """
-        if isinstance(obj, models.User):
-            instance_kwargs = {'owner_username': obj.username}
-        else:
-            instance_kwargs = obj.cache.get_cached_instance_params(obj.pk,
-                                                                   lambda: obj)
-
-        url_kwargs = {}
-        for arg_name in self.url_arg_names:
-            arg_value = instance_kwargs.get(arg_name, None)
-            if arg_value is None:
-                try:
-                    arg_value = getattr(obj, arg_name)
-                except AttributeError:
-                    raise KeyError('No arg named %r in %r' % (arg_name,
-                                                              instance_kwargs))
-            url_kwargs[arg_name] = arg_value
-        return url_kwargs
-
-
-def api_reverse(view_name, kwargs={}, request=None, format=None):
-    """
-    A special case of URL reversal where we know we're getting an API URL. This
-    can be much faster than Django's built-in general purpose regex resolver.
-
-    """
-    if request:
-        url = '{}://{}/api/v2'.format(request.scheme, request.get_host())
-    else:
-        url = '/api/v2'
-
-    route_template_strings = {
-        'submission-detail': '/{owner_username}/datasets/{dataset_slug}/places/{place_id}/{submission_set_name}/{submission_id}',
-        'submission-list': '/{owner_username}/datasets/{dataset_slug}/places/{place_id}/{submission_set_name}',
-
-        'place-detail':
-        '/{owner_username}/datasets/{dataset_slug}/places/{place_id}',
-        'place-list': '/{owner_username}/datasets/{dataset_slug}/places',
-        'place-tag-list': '/{owner_username}/datasets/{dataset_slug}/places/{place_id}/tags',
-
-        'dataset-detail': '/{owner_username}/datasets/{dataset_slug}',
-        'user-detail': '/{owner_username}',
-        'dataset-submission-list': '/{owner_username}/datasets/{dataset_slug}/{submission_set_name}',
-        'attachment-detail': '/{owner_username}/datasets/{dataset_slug}/places/{place_id}/attachments/{attachment_id}',
-    }
-
-    try:
-        route_template_string = route_template_strings[view_name]
-    except KeyError:
-        raise ValueError('No API route named {} formatted.'.format(view_name))
-
-    url_params = dict([(key, urlquote_plus(val))
-                       for key, val in kwargs.iteritems()])
-    url += route_template_string.format(**url_params)
-
-    if format is not None:
-        url += '.' + format
-
-    return url
-
-
-class ShareaboutsRelatedField (ShareaboutsFieldMixin,
-                               serializers.HyperlinkedRelatedField):
-    """
-    Represents a Shareabouts relationship using hyperlinking.
-    """
-    read_only = True
-    view_name = None
-
-    def __init__(self, *args, **kwargs):
-        if self.view_name is not None:
-            kwargs['view_name'] = self.view_name
-        if self.queryset is not None:
-            kwargs['queryset'] = self.queryset
-        super(ShareaboutsRelatedField, self).__init__(*args, **kwargs)
-
-    def get_attribute(self, obj):
-        # Pass the entire object through to `to_representation()`,
-        # instead of the standard attribute lookup. Otherwise,
-        # obj is just a DRF relations.PKOnlyObject.
-        return obj
-
-    def to_representation(self, obj, request=None, format=None):
-        view_name = self.view_name
-        request = request if request else self.context.get('request', None)
-        format = format if format else self.format or self.context.get('format', None)
-
-        pk = getattr(obj, 'pk', None)
-        if pk is None:
-            return
-
-        kwargs = self.get_url_kwargs(obj)
-        return api_reverse(view_name, kwargs=kwargs, request=request,
-                           format=format)
-
-
-class DataSetRelatedField (ShareaboutsRelatedField):
-    view_name = 'dataset-detail'
-    url_arg_names = ('owner_username', 'dataset_slug')
-
-    def get_url(self, obj, request):
-        url_kwargs = {
-            'owner_username': obj.owner.username,
-            'dataset_slug': obj.slug,
-        }
-        return reverse(self.view_name, kwargs=url_kwargs, request=request)
-
-    def get_object(self, view_name, view_args, view_kwargs):
-        lookup_kwargs = {
-            'display_name': view_kwargs['dataset_slug'],
-            'owner__username': view_kwargs['owner_username'],
-        }
-        return self.get_queryset().get(**lookup_kwargs)
-
-
-class DataSetKeysRelatedField (ShareaboutsRelatedField):
-    view_name = 'apikey-list'
-    url_arg_names = ('owner_username', 'dataset_slug')
-
-
-class UserRelatedField (ShareaboutsRelatedField):
-    view_name = 'user-detail'
-    url_arg_names = ('owner_username',)
-
-
-class PlaceRelatedField (ShareaboutsRelatedField):
-    view_name = 'place-detail'
-    url_arg_names = ('owner_username', 'dataset_slug', 'place_id')
-    queryset = models.Place.objects.all()
-
-    def get_object(self, view_name, view_args, view_kwargs):
-        lookup_kwargs = {
-            'id': view_kwargs['place_id'],
-        }
-        return self.get_queryset().get(**lookup_kwargs)
-
-
-class SubmissionSetRelatedField (ShareaboutsRelatedField):
-    view_name = 'submission-list'
-    url_arg_names = ('owner_username', 'dataset_slug', 'place_id',
-                     'submission_set_name')
-
-
-# TODO: enable this with caching
-class TagRelatedField (serializers.HyperlinkedRelatedField):
-    view_name = 'tag-detail'
-    queryset = models.Tag.objects.all()
-    lookup_field = 'id'
-
-    def get_url(self, obj, view_name, request, format):
-        url_kwargs = {
-            'owner_username': obj.dataset.owner.get_username(),
-            'dataset_slug': obj.dataset.slug,
-            'tag_id': obj.pk
-        }
-        return reverse(self.view_name, kwargs=url_kwargs, request=request, format=format)
-
-    # TODO: do we need this?
-    def get_object(self, view_name, view_args, view_kwargs):
-        lookup_kwargs = {
-           'dataset__slug': view_kwargs['dataset_slug'],
-           'id': view_kwargs['tag_id']
-        }
-        return self.get_queryset().get(**lookup_kwargs)
-
-
-class ShareaboutsIdentityField (ShareaboutsFieldMixin,
-                                serializers.HyperlinkedIdentityField):
-    read_only = True
-
-    def __init__(self, *args, **kwargs):
-        view_name = kwargs.pop('view_name', None) or getattr(self, 'view_name',
-                                                             None)
-        super(ShareaboutsIdentityField, self).__init__(view_name=view_name,
-                                                       *args, **kwargs)
-
-    def get_attribute(self, obj):
-        # Pass the entire object through to `to_representation()`,
-        # instead of the standard attribute lookup. Otherwise,
-        # obj is just a DRF relations.PKOnlyObject.
-        return obj
-
-    def to_representation(self, obj, request=None, format=None):
-        if obj.pk is None:
-            return None
-
-        request = request if request else self.context.get('request', None)
-        format = format if format else self.context.get('format', None)
-
-        view_name = self.view_name or self.parent.opts.view_name
-
-        kwargs = self.get_url_kwargs(obj)
-
-        if format and self.format and self.format != format:
-            format = self.format
-
-        return api_reverse(view_name, kwargs=kwargs, request=request,
-                           format=format)
-
-
-class PlaceIdentityField (ShareaboutsIdentityField):
-    url_arg_names = ('owner_username', 'dataset_slug', 'place_id')
-    view_name = 'place-detail'
-
-
-class AttachmentIdentityField (ShareaboutsIdentityField):
-    url_arg_names = ('owner_username', 'dataset_slug', 'place_id', 'attachment_id')
-    view_name = 'attachment-detail'
-
-
-class SubmissionSetIdentityField (ShareaboutsIdentityField):
-    url_arg_names = ('owner_username', 'dataset_slug', 'place_id',
-                     'submission_set_name')
-    view_name = 'submission-list'
-
-
-class DataSetPlaceSetIdentityField (ShareaboutsIdentityField):
-    url_arg_names = ('owner_username', 'dataset_slug')
-    view_name = 'place-list'
-
-
-class DataSetSubmissionSetIdentityField (ShareaboutsIdentityField):
-    url_arg_names = ('owner_username', 'dataset_slug', 'submission_set_name')
-    view_name = 'dataset-submission-list'
-
-
-class SubmissionIdentityField (ShareaboutsIdentityField):
-    url_arg_names = ('owner_username', 'dataset_slug', 'place_id',
-                     'submission_set_name', 'submission_id')
-    view_name = 'submission-detail'
-
-
-class TagIdentityField (serializers.HyperlinkedIdentityField):
-    lookup_field = 'id'
-
-    def __init__(self, *args, **kwargs):
-        super(TagIdentityField, self).__init__(view_name='tag-detail', *args, **kwargs)
-
-    def get_url(self, obj, view_name, request, format):
-        url_kwargs = {
-            'owner_username': obj.dataset.owner.get_username(),
-            'dataset_slug': obj.dataset.slug,
-            'tag_id': obj.pk
-        }
-        return reverse(view_name, kwargs=url_kwargs, request=request, format=format)
-
-
-class PlaceTagListIdentityField (ShareaboutsIdentityField):
-    url_arg_names = ('owner_username', 'dataset_slug', 'place_id',)
-    view_name = 'place-tag-list'
-
-
-class PlaceTagIdentityField (serializers.HyperlinkedIdentityField):
-    lookup_field = 'id'
-
-    def __init__(self, *args, **kwargs):
-        super(PlaceTagIdentityField, self).__init__(view_name='place-tag-detail', *args, **kwargs)
-
-    def get_url(self, obj, view_name, request, format):
-        url_kwargs = {
-            'owner_username': obj.tag.dataset.owner.get_username(),
-            'dataset_slug': obj.tag.dataset.slug,
-            'place_id': obj.place.id,
-            'place_tag_id': obj.pk
-        }
-        return reverse(view_name, kwargs=url_kwargs, request=request, format=format)
-
-
-class DataSetIdentityField (ShareaboutsIdentityField):
-    url_arg_names = ('owner_username', 'dataset_slug')
-    view_name = 'dataset-detail'
-
-
-class AttachmentFileField (serializers.FileField):
-    def to_representation(self, obj):
-        return obj.storage.url(obj.name)
-
-
-###############################################################################
-#
 # User Data Strategies
 # --------------------
 # Shims for reading user data from various social authentication provider
 # objects.
 #
+
 
 class DefaultUserDataStrategy (object):
     def extract_avatar_url(self, user_info):
@@ -381,9 +69,9 @@ class DefaultUserDataStrategy (object):
 class TwitterUserDataStrategy (object):
     def extract_avatar_url(self, user_info):
         try:
-          url = user_info['profile_image_url_https']
+            url = user_info['profile_image_url_https']
         except:
-          url = user_info['profile_image_url']
+            url = user_info['profile_image_url']
 
         url_pattern = '^(?P<path>.*?)(?:_normal|_mini|_bigger|)(?P<ext>\.[^\.]*)$'
         match = re.match(url_pattern, url)
@@ -413,8 +101,8 @@ class FacebookUserDataStrategy (object):
 
 class GoogleUserDataStrategy (object):
     def extract_avatar_url(self, user_info):
-       url = user_info['image']['url']
-       return url
+        url = user_info['image']['url']
+        return url
 
     def extract_full_name(self, user_info):
         name = user_info['name']['givenName'] + ' ' + user_info['name']['familyName']
