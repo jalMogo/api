0b2d13c1840b8a0306934cfc85ad6d9cd3c8b0e7
jalmogo <you@example.com>
2019-04-24 18:20:33 -0700

    feat(Flavor): add model and serializer

---
+++ src/sa_api_v2/models/flavor.py
@@ -0,0 +1,30 @@
+from django.contrib.gis.db import models
+
+
+class Flavor(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    # TODO: add datasets
+    # datasets = models.ManyToManyField(DataSet, related_name='tags', on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return "{name}".format(name=self.name)
+
+    class Meta:
+        app_label = 'sa_api_v2'
+        db_table = 'ms_api_flavor'
+        ordering = ['name']
+
+
+class Category(models.Model):
+    icon = models.CharField(max_length=128)
+    flavor = models.ForeignKey(Flavor, related_name='categories',
+                               on_delete=models.CASCADE)
+    # TODO, once Form model is created:
+    # form = models.ForeignKey(Form, related_name='categories',
+    # on_delete=models.CASCADE)
+
+    class Meta:
+        app_label = 'sa_api_v2'
+        db_table = 'ms_api_category'
+        # ordering = ['name']
