3487447234e7e36945c9c39df7c795a7de4e3b06
jalmogo <you@example.com>
2019-05-01 13:53:46 -0700

    refactor(FlavorInstanceView): use django shortcuts

--- src/sa_api_v2/views/flavors.py
+++ src/sa_api_v2/views/flavors.py
@@ -1,8 +1,8 @@
 from .. import models
 from django.db.models import Prefetch
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from .. import serializers
-from django.http import Http404
 
 
 class FlavorInstanceView (generics.RetrieveAPIView):
@@ -11,33 +11,24 @@ class FlavorInstanceView (generics.RetrieveAPIView):
     ---
     Get a particular Flavor
 
-    **Authentication**: Basic, session, or key auth *(optional)*
-
     """
 
     model = models.Flavor
     serializer_class = serializers.FlavorSerializer
 
-    # TODO: rename this to slug
-    # TODO: refacto this to use django's shortcuts
-    def get_object_or_404(self, name):
-        try:
-            return self.model.objects\
-                .filter(name=name)\
-                .prefetch_related(
-                    Prefetch(
-                        'forms__modules',
-                        queryset=models.FormModule.objects.select_related(
-                            'radiofield',
-                            # 'checkboxfield',
-                        ),
-                    ),
-                )\
-                .get()
-        except self.model.DoesNotExist:
-            raise Http404
-
     def get_object(self, queryset=None):
-        flavor_slug = self.kwargs['flavor_name']
-        obj = self.get_object_or_404(flavor_slug)
+        flavor_name = self.kwargs['flavor_name']
+        flavor_queryset = self.model\
+                              .objects\
+                              .filter(name=flavor_name)\
+                              .prefetch_related(
+                                  Prefetch(
+                                      'forms__modules',
+                                      queryset=models.FormModule.objects.select_related(
+                                          'radiofield',
+                                      ),
+                                  ),
+                              )
+
+        obj = get_object_or_404(flavor_queryset)
         return obj
