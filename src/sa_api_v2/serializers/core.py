c2df9496e6884e1dcfeff1e365329d79d9371a77
jalmogo <you@example.com>
2019-05-08 16:33:36 -0700

    feat(FormSerializer): remove 'null' fields from FormModule

--- src/sa_api_v2/serializers/core.py
+++ src/sa_api_v2/serializers/core.py
@@ -5,6 +5,9 @@ from django.utils import six
 from collections import defaultdict
 from django.core.exceptions import ValidationError
 from rest_framework import serializers
+from collections import OrderedDict
+from rest_framework.fields import SkipField
+from rest_framework.relations import PKOnlyObject
 
 from .mixins import (
     ActivityGenerator,
@@ -755,7 +758,38 @@ class FormModuleSerializer (serializers.ModelSerializer):
 
     class Meta:
         model = models.FormModule
-        exclude = ['form', 'order']
+        exclude = ['form']
+
+    # removes "null" fields
+    def to_representation(self, instance):
+        """
+        Object instance -> Dict of primitive datatypes.
+        """
+        ret = OrderedDict()
+        fields = self._readable_fields
+
+        for field in fields:
+            try:
+                attribute = field.get_attribute(instance)
+            except SkipField:
+                continue
+
+            # KEY IS HERE:
+            if attribute in [None, '']:
+                continue
+
+            # We skip `to_representation` for `None` values so that fields do
+            # not have to explicitly deal with that case.
+            #
+            # For related fields with `use_pk_only_optimization` we need to
+            # resolve the pk value.
+            check_for_none = attribute.pk if isinstance(attribute, PKOnlyObject) else attribute
+            if check_for_none is None:
+                ret[field.field_name] = None
+            else:
+                ret[field.field_name] = field.to_representation(attribute)
+
+        return ret
 
 
 class FormSerializer (serializers.ModelSerializer):
