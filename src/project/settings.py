af970422e899fdabccfecb2ab1ca9aadf2a37c95
jalMogo <42042460+jalMogo@users.noreply.github.com>
2019-07-24 16:24:23 -0700

    Jalmogo/upgrade social auth (#15)
    * Revert "Revert "Merge pull request #14 from jalMogo/jalmogo/upgrade-auth-deps""
    
    This reverts commit c5646c9eb903d754ab34c8600decc22b24e5fc0f.
    
    * fix(UserSerializer)

--- src/project/settings.py
+++ src/project/settings.py
@@ -233,7 +233,7 @@ SOCIAL_AUTH_PROTECTED_USER_FIELDS = ['email',]
 
 SOCIAL_AUTH_FACEBOOK_EXTRA_DATA = ['name', 'picture', 'about']
 SOCIAL_AUTH_TWITTER_EXTRA_DATA = ['name', 'description', 'profile_image_url_https']
-SOCIAL_AUTH_GOOGLE_OAUTH2_EXTRA_DATA = ['name', 'image', 'aboutMe']
+SOCIAL_AUTH_GOOGLE_OAUTH2_EXTRA_DATA = ['name', 'aboutMe', 'picture']
 
 # Explicitly request the following extra things from facebook
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {'fields': 'id,name,picture.width(96).height(96),first_name,last_name,about'}
