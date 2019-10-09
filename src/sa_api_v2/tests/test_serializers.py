41c55c86ce9c0d8897812419201341da1af25945
jalmogo <you@example.com>
2019-10-09 13:39:55 -0700

    add garfield form/flavor

--- src/sa_api_v2/tests/test_serializers.py
+++ src/sa_api_v2/tests/test_serializers.py
@@ -587,6 +587,8 @@ class TestFlavorDeserializer (TestCase):
                                               owner_id=cls.owner.id)
         DataSet.objects.create(slug='columbia-input',
                                               owner_id=cls.owner.id)
+        DataSet.objects.create(slug='garfield-input',
+                                              owner_id=cls.owner.id)
         pbdurham_projects = DataSet.objects.create(slug='pbdurham-projects',
                                               owner_id=cls.owner.id)
         Group.objects.create(
